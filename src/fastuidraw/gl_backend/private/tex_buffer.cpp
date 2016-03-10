c313b36a54dded53cf2c5cac6bed1d2824b69224
Kevin Rogovin <kevin.rogovin@intel.com>
2016-03-10 10:27:18 +0200

    gl_backend/private: add missing copyright notices

--- src/fastuidraw/gl_backend/private/tex_buffer.cpp
+++ src/fastuidraw/gl_backend/private/tex_buffer.cpp
@@ -1,3 +1,21 @@
+/*!
+ * \file tex_buffer.cpp
+ * \brief file tex_buffer.hpp
+ *
+ * Copyright 2016 by Intel.
+ *
+ * Contact: kevin.rogovin@intel.com
+ *
+ * This Source Code Form is subject to the
+ * terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with
+ * this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *
+ * \author Kevin Rogovin <kevin.rogovin@intel.com>
+ *
+ */
+
 #include <fastuidraw/gl_backend/gl_context_properties.hpp>
 #include "tex_buffer.hpp"
 
--- src/fastuidraw/gl_backend/private/texture_gl.cpp
+++ src/fastuidraw/gl_backend/private/texture_gl.cpp
@@ -1,3 +1,21 @@
+/*!
+ * \file texture_gl.cpp
+ * \brief file texture_gl.cpp
+ *
+ * Copyright 2016 by Intel.
+ *
+ * Contact: kevin.rogovin@intel.com
+ *
+ * This Source Code Form is subject to the
+ * terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with
+ * this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *
+ * \author Kevin Rogovin <kevin.rogovin@intel.com>
+ *
+ */
+
 #include <iostream>
 #include <fastuidraw/gl_backend/gl_context_properties.hpp>
 #include <fastuidraw/gl_backend/gl_get.hpp>
