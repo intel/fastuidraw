f814f9477fc458fa54c179282f8e610d46ca25a8
Kevin Rogovin <kevin.rogovin@intel.com>
2016-07-05 13:17:28 +0300

    demos: tweaks for better code consistency

--- demos/common/random.cpp
+++ demos/common/random.cpp
@@ -1,19 +1,26 @@
 #include <stdlib.h>
 #include "random.hpp"
 
-float
-random_value(float pmin, float pmax)
+static
+void
+init_srand(void)
 {
-  int v;
-  float r;
   static bool inited(false);
-
   if(!inited)
     {
       inited = true;
       srand(0);
     }
+}
 
+
+float
+random_value(float pmin, float pmax)
+{
+  int v;
+  float r;
+
+  init_srand();
   v = rand();
   r = static_cast<float>(v) / static_cast<float>(RAND_MAX);
   return pmin + r * (pmax - pmin);
