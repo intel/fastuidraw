18964aac0e0bb9da67d5cd9b4e3148b8ce880dfa
Kevin Rogovin <kevin.rogovin@intel.com>
2016-07-06 16:54:15 +0300

    cairo_painter_cells: tweak test to clear and draw a moving rect

--- cairo_painter_cells/test.cpp
+++ cairo_painter_cells/test.cpp
@@ -1,10 +1,15 @@
 #include "sdl_cairo_demo.hpp"
 #include "random.hpp"
+#include "simple_time.hpp"
 
 class test:public sdl_cairo_demo
 {
 public:
-  test(void)
+  test(void):
+    m_x(0.0),
+    m_y(0.0),
+    m_dx(100.0),
+    m_dy(100.0)
   {}
 
   ~test()
@@ -45,16 +50,48 @@ protected:
   {
     std::pair<int, int> wh;
     double r, g, b;
+    int64_t delta_time_us;
+    double delta_time_s, dx, dy;
 
-    r = random_value(0.0, 1.0);
-    g = random_value(0.0, 1.0);
-    b = random_value(0.0, 1.0);
+    delta_time_us = m_timer.restart_us();
+    delta_time_s = static_cast<double>(delta_time_us) / 1e6;
+    r = 0.0;
+    g = 1.0;
+    b = 1.0;
     wh = dimensions();
+
+    cairo_set_operator(m_cairo, CAIRO_OPERATOR_SOURCE);
+    cairo_set_source_rgb(m_cairo, 0.5, 0.5, 0.5);
+    cairo_paint(m_cairo);
+
+    cairo_set_operator(m_cairo, CAIRO_OPERATOR_OVER);
     cairo_new_path(m_cairo);
-    cairo_rectangle(m_cairo, 0.0, 0.0, wh.first, wh.second);
+    cairo_rectangle(m_cairo, m_x - 50.0, m_y - 50.0, 100.0, 100.0);
     cairo_set_source_rgb(m_cairo, r, g, b);
     cairo_fill(m_cairo);
+
+    dx = delta_time_s * m_dx;
+    dy = delta_time_s * m_dy;
+
+    if(m_x + dx > wh.first || m_x + m_dx < 0.0)
+      {
+        m_dx = -m_dx;
+        dx = -dx;
+      }
+
+    if(m_y + dy > wh.second || m_y + m_dy < 0.0)
+      {
+        m_dy = -m_dy;
+        dy = -dy;
+      }
+
+    m_x += dx;
+    m_y += dy;
   }
+
+private:
+  double m_x, m_y, m_dx, m_dy;
+  simple_time m_timer;
 };
 
 int
