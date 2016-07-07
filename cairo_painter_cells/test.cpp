45d6928ad69fe7b1e9b4e4b4a6c1138d5f84ce67
Kevin Rogovin <kevin.rogovin@intel.com>
2016-07-07 12:24:14 +0300

    cairo_painter_cells: test image blitting

--- cairo_painter_cells/test.cpp
+++ cairo_painter_cells/test.cpp
@@ -2,19 +2,34 @@
 #include "random.hpp"
 #include "simple_time.hpp"
 #include "ScaleTranslate.hpp"
+#include "ImageLoader.hpp"
 
 class test:public sdl_cairo_demo
 {
 public:
   test(void):
+    m_image_file("", "image", "Image to draw to moving rectangle", *this),
     m_x(0.0),
     m_y(0.0),
     m_dx(100.0),
-    m_dy(100.0)
+    m_dy(100.0),
+    m_image(NULL),
+    m_pattern(NULL),
+    m_pattern_dims(0.0, 0.0)
   {}
 
   ~test()
-  {}
+  {
+    if(m_pattern)
+      {
+        cairo_pattern_destroy(m_pattern);
+      }
+
+    if(m_image)
+      {
+        cairo_surface_destroy(m_image);
+      }
+  }
 
 protected:
 
@@ -47,18 +62,29 @@ protected:
 
   virtual
   void
+  derived_init(int, int)
+  {
+    m_image = create_image_from_file(m_image_file.m_value);
+    if(m_image)
+      {
+        m_pattern = cairo_pattern_create_for_surface(m_image);
+        m_pattern_dims.x() = cairo_image_surface_get_width(m_image);
+        m_pattern_dims.y() = cairo_image_surface_get_height(m_image);
+      }
+  }
+
+  virtual
+  void
   draw_frame(void)
   {
+    cairo_save(m_cairo);
+
     std::pair<int, int> wh;
-    double r, g, b;
     int64_t delta_time_us;
     double delta_time_s, dx, dy;
 
     delta_time_us = m_timer.restart_us();
     delta_time_s = static_cast<double>(delta_time_us) / 1e6;
-    r = 0.0;
-    g = 1.0;
-    b = 1.0;
     wh = dimensions();
 
     cairo_set_operator(m_cairo, CAIRO_OPERATOR_SOURCE);
@@ -66,10 +92,24 @@ protected:
     cairo_paint(m_cairo);
 
     cairo_set_operator(m_cairo, CAIRO_OPERATOR_OVER);
-    cairo_new_path(m_cairo);
-    cairo_rectangle(m_cairo, m_x - 50.0, m_y - 50.0, 100.0, 100.0);
-    cairo_set_source_rgb(m_cairo, r, g, b);
-    cairo_fill(m_cairo);
+    if(m_pattern)
+      {
+        cairo_translate(m_cairo, m_x - m_pattern_dims.x() * 0.5, m_y - m_pattern_dims.y() * 0.5);
+        cairo_set_source(m_cairo, m_pattern);
+        cairo_rectangle(m_cairo, 0.0, 0.0, m_pattern_dims.x(), m_pattern_dims.y());
+        cairo_fill(m_cairo);
+      }
+    else
+      {
+        double r, g, b;
+        r = 0.0;
+        g = 1.0;
+        b = 1.0;
+        cairo_new_path(m_cairo);
+        cairo_rectangle(m_cairo, m_x - 50.0, m_y - 50.0, 100.0, 100.0);
+        cairo_set_source_rgb(m_cairo, r, g, b);
+        cairo_fill(m_cairo);
+      }
 
     dx = delta_time_s * m_dx;
     dy = delta_time_s * m_dy;
@@ -88,11 +128,18 @@ protected:
 
     m_x += dx;
     m_y += dy;
+
+    cairo_restore(m_cairo);
   }
 
 private:
+  command_line_argument_value<std::string> m_image_file;
+
   double m_x, m_y, m_dx, m_dy;
   simple_time m_timer;
+  cairo_surface_t *m_image;
+  cairo_pattern_t *m_pattern;
+  vec2 m_pattern_dims;
 };
 
 int
