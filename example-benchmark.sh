02894bfcc6932d9170bb1fc526011bf894a98680
Kevin Rogovin <kevin.rogovin@intel.com>
2016-07-19 12:58:29 +0300

    example-benchmark: common swap_interval for all painter-cell ports

--- example-benchmark.sh
+++ example-benchmark.sh
@@ -9,8 +9,9 @@ FULLSCREEN="true"
 WIDTH="1920"
 HEIGHT="1080"
 WIDTH_HEIGHT="$WIDTH""x""$HEIGHT"
+SWAP_INTERVAL="0"
 
-common_options="fullscreen $FULLSCREEN add_image /usr/share/games/blobwars/gfx/ num_cells_x 25 num_cells_y 12 table_width 4000 table_height 2000 num_frames 100 num_skip_frames 3 init_table_rotating true init_table_clipped true init_cell_rotating true num_background_colors 100"
+common_options="fullscreen $FULLSCREEN swap_interval $SWAP_INTERVAL add_image /usr/share/games/blobwars/gfx/ num_cells_x 25 num_cells_y 12 table_width 4000 table_height 2000 num_frames 100 num_skip_frames 3 init_table_rotating true init_table_clipped true init_cell_rotating true num_background_colors 100"
 
 
 function run_benchmark {
