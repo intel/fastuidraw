941abeacb2e1b18570b0c40f79c5b680d1a05665
Kevin Rogovin <kevin.rogovin@intel.com>
2016-07-25 11:48:05 +0300

    example-benchmark: make more of the options as variables

--- example-benchmark.sh
+++ example-benchmark.sh
@@ -3,15 +3,33 @@
 # destination directory to place results
 RESULTS="."
 
+# how many frames to run the benchmark
+NUM_FRAMES="100"
+
 #can be true or false
 FULLSCREEN="true"
 
+#screen/window resolution
 WIDTH="1920"
 HEIGHT="1080"
-WIDTH_HEIGHT="$WIDTH""x""$HEIGHT"
+
+# 0 means tell SDL (or Qt) to disable vsync
 SWAP_INTERVAL="0"
 
-common_options="fullscreen $FULLSCREEN swap_interval $SWAP_INTERVAL add_image /usr/share/games/blobwars/gfx/ num_cells_x 25 num_cells_y 12 table_width 4000 table_height 2000 num_frames 100 num_skip_frames 3 init_table_rotating true init_table_clipped true init_cell_rotating true num_background_colors 100"
+# number of cells in each dimension to draw
+NUM_CELLS_X="25"
+NUM_CELLS_Y="12"
+
+# virtual size of table, display is zoomed out to show
+# entire table.
+TABLE_WIDTH="4000"
+TABLE_HEIGHT="2000"
+
+
+# for Qt's -geometry bits
+WIDTH_HEIGHT="$WIDTH""x""$HEIGHT"
+
+common_options="fullscreen $FULLSCREEN swap_interval $SWAP_INTERVAL add_image /usr/share/games/blobwars/gfx/ num_cells_x $NUM_CELLS_X num_cells_y $NUM_CELLS_Y table_width $TABLE_WIDTH table_height $TABLE_HEIGHT num_frames $NUM_FRAMES num_skip_frames 3 init_table_rotating true init_table_clipped true init_cell_rotating true num_background_colors 100"
 
 
 function run_benchmark {
