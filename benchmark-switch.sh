ef24ac0c2c384220c121aa15b85ac0abfca6575b
Kevin Rogovin <kevin.rogovin@intel.com>
2016-08-25 09:47:00 +0300

    add benchmarker for benchmarking switch performace on GLSL uber-shader

---
+++ benchmark-switch.sh
@@ -0,0 +1,55 @@
+#/bin/bash
+
+# destination directory to place results
+RESULTS="results"
+VERT_USE_SWITCH="false"
+FRAG_USE_SWITCH="true"
+BLEND_USE_SWITCH="true"
+
+# how many frames to run the benchmark
+NUM_FRAMES="1000"
+
+#can be true or false
+FULLSCREEN="true"
+
+#screen/window resolution
+WIDTH="1920"
+HEIGHT="1080"
+
+# 0 means tell SDL (or Qt) to disable vsync
+SWAP_INTERVAL="0"
+
+# number of cells in each dimension to draw
+NUM_CELLS_X="25"
+NUM_CELLS_Y="12"
+
+# virtual size of table, display is zoomed out to show
+# entire table.
+TABLE_WIDTH="4000"
+TABLE_HEIGHT="2000"
+
+
+# for Qt's -geometry bits
+WIDTH_HEIGHT="$WIDTH""x""$HEIGHT"
+
+common_options="fullscreen $FULLSCREEN swap_interval $SWAP_INTERVAL add_image /usr/share/games/blobwars/gfx/ num_cells_x $NUM_CELLS_X num_cells_y $NUM_CELLS_Y table_width $TABLE_WIDTH table_height $TABLE_HEIGHT num_frames $NUM_FRAMES num_skip_frames 3 init_table_rotating true init_table_clipped true init_cell_rotating true num_background_colors 100"
+
+shader_options="uber_vert_use_switch $VERT_USE_SWITCH uber_frag_use_switch $FRAG_USE_SWITCH uber_blend_use_switch $BLEND_USE_SWITCH"
+
+function run_benchmark {
+    mkdir -p $RESULTS
+    echo "vblank_mode=0 $1 $common_options $2 > $RESULTS/$3"
+    vblank_mode=0 $1 $common_options $2 > $RESULTS/$3
+}
+
+#run_benchmark qt_painter_cells/qt-painter-cells "-geometry $WIDTH_HEIGHT" qt-glwidget-results.txt
+#run_benchmark qt_painter_cells/qt-painter-cells "-geometry $WIDTH_HEIGHT use_gl_widget false -graphicssystem raster" qt-nonglwidget-raster-results.txt
+#run_benchmark qt_painter_cells/qt-painter-cells "-geometry $WIDTH_HEIGHT use_gl_widget false -graphicssystem native" qt-nonglwidget-native-results.txt
+#run_benchmark qt_painter_cells/qt-painter-cells "-geometry $WIDTH_HEIGHT use_gl_widget false -graphicssystem opengl" qt-nonglwidget-gl-results.txt
+#run_benchmark cairo_painter_cells/cairo-painter-cells-release "width $WIDTH height $HEIGHT cairo_backend xlib_offscreen" cairo-x-offscreen-results.txt
+#run_benchmark cairo_painter_cells/cairo-painter-cells-release "width $WIDTH height $HEIGHT cairo_backend xlib_onscreen" cairo-x-onscreen-results.txt
+#run_benchmark cairo_painter_cells/cairo-painter-cells-release "width $WIDTH height $HEIGHT cairo_backend offscreen_data" cairo-cpu-offscreen-results.txt
+#run_benchmark cairo_painter_cells/cairo-painter-cells-release "width $WIDTH height $HEIGHT cairo_backend gl" cairo-gl-results.txt
+#run_benchmark skia_painter_cells/skia-painter-cells-release "width $WIDTH height $HEIGHT" skia-results.txt
+run_benchmark ./painter-cells-GL-release "width $WIDTH height $HEIGHT $shader_options" fastuidraw-results.txt
+run_benchmark ./painter-cells-GLES-release "width $WIDTH height $HEIGHT $shader_options" fastuidraw-es-results.txt
