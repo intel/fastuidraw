63d86f9b0d31350d32368e45bdbd1d53c8e44832
Kevin Rogovin <kevin.rogovin@intel.com>
2016-08-26 11:52:23 +0300

    Makefiles's: comment the reason for the contents of the .pre.mk files

--- Makefile.base.pre.mk
+++ Makefile.base.pre.mk
@@ -1,3 +1,7 @@
+# We need to initialize the variables with :=
+# so that as GNU make walks the directory structure
+# it correctly observes the output of $(call filelist)
+# to get paths correct.
 CLEAN_FILES :=
 SUPER_CLEAN_FILES :=
 
--- Makefile.gl_backend.pre.mk
+++ Makefile.gl_backend.pre.mk
@@ -1,3 +1,7 @@
+# We need to initialize the variables with :=
+# so that as GNU make walks the directory structure
+# it correctly observes the output of $(call filelist)
+# to get paths correct.
 LIBRARY_GL_SOURCES :=
 LIBRARY_GL_RESOURCE_STRING :=
 LIBRARY_PRIVATE_GL_SOURCES :=
