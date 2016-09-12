4e6cf6c22ceffefd6f3ef72791d5bfc19a18cab6
Kevin Rogovin <kevin.rogovin@intel.com>
2016-09-12 17:56:09 +0300

    fastuidraw/glsl, gl_backend: minor header cleanup + add copyright notices

--- src/fastuidraw/gl_backend/gluniform_implement.cpp
+++ src/fastuidraw/gl_backend/gluniform_implement.cpp
@@ -1,3 +1,21 @@
+/*!
+ * \file gluniform_implement.cpp
+ * \brief file gluniform_implement.cpp
+ *
+ * Copyright 2016 by Intel.
+ *
+ * Contact: kevin.rogovin@intel.com
+ *
+ * This Source Code Form is subject to the
+ * terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with
+ * this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *
+ * \author Kevin Rogovin <kevin.rogovin@intel.com>
+ *
+ */
+
 #include <fastuidraw/gl_backend/ngl_header.hpp>
 #include <fastuidraw/util/vecN.hpp>
 #include <fastuidraw/util/matrix.hpp>
