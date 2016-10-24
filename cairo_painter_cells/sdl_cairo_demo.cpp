fc6a95bbe6792e71c3a921d1ee5f7ae643f76511
Kevin Rogovin <kevin.rogovin@intel.com>
2016-10-24 11:56:55 +0300

    cairo_painter_cells: default cairo backend to CPU backend

--- cairo_painter_cells/sdl_cairo_demo.cpp
+++ cairo_painter_cells/sdl_cairo_demo.cpp
@@ -55,7 +55,7 @@ sdl_cairo_demo(const std::string &about_text):
   m_width(800, "width", "window width", *this),
   m_height(480, "height", "window height", *this),
   m_show_framerate(false, "show_framerate", "if true show the cumulative framerate at end", *this),
-  m_backend(backend_cairo_xlib_off_screen,
+  m_backend(backend_cairo_offscreen_data_surface,
             enumerated_string_type<enum backend_cairo_t>()
             .add_entry("xlib_onscreen", backend_cairo_xlib_on_screen,
                        "render directly to X window surface")
