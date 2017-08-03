67417c0c4eed9ceb861b5ef4843ec61ea83e2bc8
Kevin Rogovin <kevin.rogovin@intel.com>
2017-08-03 23:59:09 +0300

    fastuidraw/gl_backend/ngl_generator: place built files into build/ + simplify .gitignore

--- src/fastuidraw/gl_backend/ngl_generator/Rules.mk
+++ src/fastuidraw/gl_backend/ngl_generator/Rules.mk
@@ -4,8 +4,9 @@ dirstack_$(sp)	:= $(d)
 d		:= $(dir)
 # End standard header
 
-NGL_FILTER := $(call filelist, filter)
-NGL_EXTRACTOR := $(call filelist, extractor)
+NGL_BUILD := build/ngl_generator
+NGL_FILTER := $(NGL_BUILD)/filter
+NGL_EXTRACTOR := $(NGL_BUILD)/extractor
 
 ngl_generator = $(NGL_FILTER) $(NGL_FILTER)
 
@@ -13,22 +14,26 @@ NGL_EXTRACTOR_LDFLAGS :=
 NGL_LL := flex
 
 $(NGL_FILTER): $(call filelist, filter.cpp)
-	$(CXX) -o $@ $^
+	mkdir -p $(dir $@)
+	$(CXX) -o $@ $<
 
-$(NGL_EXTRACTOR): $(call filelist, gl_flex.o HeaderCreator.o)
+$(NGL_EXTRACTOR): $(NGL_BUILD)/gl_flex.o $(NGL_BUILD)/HeaderCreator.o
 	$(CXX) -o $@ $^ $(NGL_EXTRACTOR_LDFLAGS)
 
-$(call filelist, gl_flex.cpp): $(call filelist, gl_flex.fl.cpp)
-	$(NGL_LL) -o $@ $^
+$(NGL_BUILD)/gl_flex.cpp: $(call filelist, gl_flex.fl.cpp)
+	mkdir -p $(dir $@)
+	$(NGL_LL) -o $@ $<
 
-$(call filelist, gl_flex.o): $(call filelist, gl_flex.cpp, HeaderCreator.hpp)
-	$(CXX) -o $@ -c $^
+$(NGL_BUILD)/gl_flex.o: $(NGL_BUILD)/gl_flex.cpp $(call filelist, HeaderCreator.hpp)
+	mkdir -p $(dir $@)
+	$(CXX) -Isrc/fastuidraw/gl_backend/ngl_generator -o $@ -c $<
 
-$(call filelist, HeaderCreator.o): $(call filelist, HeaderCreator.cpp, HeaderCreator.hpp)
-	$(CXX) -o $@ -c $^
+$(NGL_BUILD)/HeaderCreator.o: $(call filelist, HeaderCreator.cpp HeaderCreator.hpp)
+	mkdir -p $(dir $@)
+	$(CXX) -o $@ -c $<
 
-SUPER_CLEAN_FILES += $(call filelist, extractor filter gl_flex.cpp lex.yy.c)
-SUPER_CLEAN_FILES += $(call filelist, *.o *.exe)
+SUPER_CLEAN_FILES += $(NGL_FILTER) $(NGL_EXTRACTOR)
+SUPER_CLEAN_FILES += $(NGL_BUILD)/gl_flex.cpp $(NGL_BUILD)/gl_flex.o $(NGL_BUILD)/HeaderCreator.o
 
 # Begin standard footer
 d		:= $(dirstack_$(sp))
