9d9f888c8c39866599eb401da2cc6cc4d7357abf
Kevin Rogovin <kevin.rogovin@intel.com>
2017-08-10 20:53:26 +0300

    fastuidraw/text/private: remove freetype_util*.[ch]pp

--- src/fastuidraw/text/private/Rules.mk
+++ src/fastuidraw/text/private/Rules.mk
@@ -4,7 +4,7 @@ dirstack_$(sp)	:= $(d)
 d		:= $(dir)
 # End standard header
 
-FASTUIDRAW_PRIVATE_SOURCES += $(call filelist, rect_atlas.cpp freetype_util.cpp freetype_curvepair_util.cpp)
+FASTUIDRAW_PRIVATE_SOURCES += $(call filelist, rect_atlas.cpp)
 
 # Begin standard footer
 d		:= $(dirstack_$(sp))
