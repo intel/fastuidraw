28a5ed288f3de24ce18fe678e3c3a1973f214395
Kevin Rogovin <kevin.rogovin@intel.com>
2018-05-03 23:55:58 +0300

    demos/common/simple_time: avoid using namespace std::chrono
    Change-Id: I013cdd91c2fb3de408a61fedb6962ab47b20fc28

--- demos/common/simple_time.hpp
+++ demos/common/simple_time.hpp
@@ -25,50 +25,41 @@
 
 #include <stdint.h>
 
-using namespace std::chrono;
-
 class simple_time
 {
 public:
-
   simple_time(void)
   {
-    m_start_time = steady_clock::now();
+    m_start_time = std::chrono::steady_clock::now();
   }
 
   int32_t
   elapsed(void)
   {
-    return time_difference_ms(steady_clock::now(), m_start_time);
+    return time_difference_ms(std::chrono::steady_clock::now(), m_start_time);
   }
 
   int32_t
   restart(void)
   {
-    auto current_time = steady_clock::now();
-
-    auto return_value = time_difference_ms(current_time, m_start_time);
+    auto current_time = std::chrono::steady_clock::now();
+    int32_t return_value = time_difference_ms(current_time, m_start_time);
     m_start_time = current_time;
-
     return return_value;
   }
 
-
-  //
   int64_t
   elapsed_us(void)
   {
-    return time_difference_us(steady_clock::now(), m_start_time);
+    return time_difference_us(std::chrono::steady_clock::now(), m_start_time);
   }
 
   int64_t
   restart_us(void)
   {
-    auto current_time = steady_clock::now();
-
-    auto return_value = time_difference_us(current_time, m_start_time);
+    auto current_time = std::chrono::steady_clock::now();
+    int64_t return_value = time_difference_us(current_time, m_start_time);
     m_start_time = current_time;
-
     return return_value;
   }
 
@@ -76,17 +67,19 @@ private:
 
   static
   int32_t
-  time_difference_ms(const time_point<steady_clock> &end, const time_point<steady_clock> &begin)
+  time_difference_ms(const std::chrono::time_point<std::chrono::steady_clock> &end,
+                     const std::chrono::time_point<std::chrono::steady_clock> &begin)
   {
-    return duration_cast<milliseconds>(end - begin).count();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
   }
 
   static
   int64_t
-  time_difference_us(const time_point<steady_clock> &end, const time_point<steady_clock> &begin)
+  time_difference_us(const std::chrono::time_point<std::chrono::steady_clock> &end,
+                     const std::chrono::time_point<std::chrono::steady_clock> &begin)
   {
-    return duration_cast<microseconds>(end - begin).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
   }
 
-  time_point<steady_clock> m_start_time;
+  std::chrono::time_point<std::chrono::steady_clock> m_start_time;
 };
