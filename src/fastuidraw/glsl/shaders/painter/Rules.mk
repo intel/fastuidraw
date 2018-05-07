2744a77260458adbda1853a2bee334debbb1592a
Kevin Rogovin <kevin.rogovin@intel.com>
2018-05-07 12:12:22 +0300

    fastuidraw/glsl/shaders/painter: implement arc-join stroke shaders
    Change-Id: I833eb04b32e94289058e7e66082d0e21162f159c

--- src/fastuidraw/glsl/shaders/painter/Rules.mk
+++ src/fastuidraw/glsl/shaders/painter/Rules.mk
@@ -24,6 +24,8 @@ FASTUIDRAW_RESOURCE_STRING += $(call filelist, fastuidraw_painter_main.vert.glsl
 	fastuidraw_painter_stroke_util.vert.glsl.resource_string \
 	fastuidraw_painter_stroke.vert.glsl.resource_string \
 	fastuidraw_painter_stroke.frag.glsl.resource_string \
+	fastuidraw_painter_arc_stroke.vert.glsl.resource_string \
+	fastuidraw_painter_arc_stroke.frag.glsl.resource_string \
 	fastuidraw_painter_fill.vert.glsl.resource_string \
 	fastuidraw_painter_fill.frag.glsl.resource_string \
 	fastuidraw_painter_fill_aa_fuzz.vert.glsl.resource_string \
