0715ba19fc2786aacb0109c309b3b5cc5ed6ac3d
Kevin Rogovin <kevin.rogovin@intel.com>
2018-05-11 12:37:27 +0300

    fastuidraw/glsl/shaders/painter: make dash-coverage computation to seperate function and file
    Place computation of coverage from dashed shading into a dedicate
    utility function in a utility shader file.
    
    Change-Id: Ia809f37101ba7f088c646a168dbc690c17c00efb

--- src/fastuidraw/glsl/shaders/painter/Rules.mk
+++ src/fastuidraw/glsl/shaders/painter/Rules.mk
@@ -22,6 +22,7 @@ FASTUIDRAW_RESOURCE_STRING += $(call filelist, fastuidraw_painter_main.vert.glsl
 	fastuidraw_painter_forward_declares.frag.glsl.resource_string \
 	fastuidraw_painter_main.frag.glsl.resource_string \
 	fastuidraw_painter_stroke_util.vert.glsl.resource_string \
+	fastuidraw_painter_stroke_util.frag.glsl.resource_string \
 	fastuidraw_painter_stroke.vert.glsl.resource_string \
 	fastuidraw_painter_stroke.frag.glsl.resource_string \
 	fastuidraw_painter_arc_stroke.vert.glsl.resource_string \
