99eec32bad5c78a959ce0a391f3671c02f5e447b
Kevin Rogovin <kevin.rogovin@intel.com>
2018-05-28 12:17:57 +0300

    fastuidraw/src: add copyright notices to cpp files missing them
    Change-Id: I9bb577c65bdd89cfa4a03dc2230c59f7d3f60d66

--- src/fastuidraw/painter/fill_rule.cpp
+++ src/fastuidraw/painter/fill_rule.cpp
@@ -1,3 +1,21 @@
+/*!
+ * \file fill_rule.cpp
+ * \brief file fill_rule.cpp
+ *
+ * Copyright 2018 by Intel.
+ *
+ * Contact: kevin.rogovin@intel.com
+ *
+ * This Source Code Form is subject to the
+ * terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with
+ * this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *
+ * \author Kevin Rogovin <kevin.rogovin@intel.com>
+ *
+ */
+
 #include <fastuidraw/util/util.hpp>
 #include <stddef.h>
 #include <fastuidraw/painter/fill_rule.hpp>
--- src/fastuidraw/painter/stroked_point.cpp
+++ src/fastuidraw/painter/stroked_point.cpp
@@ -1,3 +1,21 @@
+/*!
+ * \file stroked_point.cpp
+ * \brief file stroked_point.cpp
+ *
+ * Copyright 2018 by Intel.
+ *
+ * Contact: kevin.rogovin@intel.com
+ *
+ * This Source Code Form is subject to the
+ * terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with
+ * this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *
+ * \author Kevin Rogovin <kevin.rogovin@intel.com>
+ *
+ */
+
 #include <fastuidraw/painter/stroked_point.hpp>
 
 //////////////////////////////////////
