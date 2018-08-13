558e4e1c255c0347d0a0332348b33e9a62dbe672
Kevin Rogovin <kevin.rogovin@gmail.com>
2018-08-13 16:46:31 +0300

    fastuidraw/glsl/shaders/painter/blend: implement w3c_darken

--- src/fastuidraw/glsl/private/backend_shaders.cpp
+++ src/fastuidraw/glsl/private/backend_shaders.cpp
@@ -742,7 +742,9 @@ create_blend_shaders(void)
     .shader(PainterEnums::blend_w3c_screen,
             create_blend_shader("fastuidraw_fbf_w3c_screen.glsl.resource_string"))
     .shader(PainterEnums::blend_w3c_overlay,
-            create_blend_shader("fastuidraw_fbf_w3c_overlay.glsl.resource_string"));
+            create_blend_shader("fastuidraw_fbf_w3c_overlay.glsl.resource_string"))
+    .shader(PainterEnums::blend_w3c_darken,
+            create_blend_shader("fastuidraw_fbf_w3c_darken.glsl.resource_string"));
 
   return return_value;
 }
