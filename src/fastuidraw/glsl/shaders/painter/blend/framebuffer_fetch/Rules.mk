e705d9df3802fccc2a245386c24fb2b972782a75
Kevin Rogovin <kevin.rogovin@intel.com>
2018-08-15 09:25:53 +0300

    fastuidraw/glsl/shaders/painter/blend: implement w3c_color

--- src/fastuidraw/glsl/shaders/painter/blend/framebuffer_fetch/Rules.mk
+++ src/fastuidraw/glsl/shaders/painter/blend/framebuffer_fetch/Rules.mk
@@ -20,6 +20,7 @@ FASTUIDRAW_RESOURCE_STRING += $(call filelist, \
 	fastuidraw_fbf_w3c_exclusion.glsl.resource_string \
 	fastuidraw_fbf_w3c_hue.glsl.resource_string \
 	fastuidraw_fbf_w3c_saturation.glsl.resource_string \
+	fastuidraw_fbf_w3c_color.glsl.resource_string \
 	)
 
 # Begin standard footer
