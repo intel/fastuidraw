17a1ac13ebdc225c246178a9bd4c2dcbbfc0456e
Kevin Rogovin <kevin.rogovin@intel.com>
2018-09-28 23:17:49 +0530

    fsatuidraw/private: new function so compute singular values of 2x2 matrix

--- src/fastuidraw/private/Rules.mk
+++ src/fastuidraw/private/Rules.mk
@@ -4,7 +4,11 @@ dirstack_$(sp)	:= $(d)
 d		:= $(dir)
 # End standard header
 
-FASTUIDRAW_PRIVATE_SOURCES += $(call filelist, interval_allocator.cpp path_util_private.cpp clip.cpp int_path.cpp)
+FASTUIDRAW_PRIVATE_SOURCES += $(call filelist, \
+	interval_allocator.cpp \
+	path_util_private.cpp \
+	clip.cpp int_path.cpp \
+	util_private_math.cpp)
 
 # Begin standard footer
 d		:= $(dirstack_$(sp))
