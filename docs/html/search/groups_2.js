726c309a52d235ef1a05086272222e272046f2c5
Kevin Rogovin <kevin.rogovin@intel.com>
2018-11-08 08:54:32 +0200

    Update doxygen content

--- docs/html/search/groups_2.js
+++ docs/html/search/groups_2.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['painter',['Painter',['../d3/d46/group___painter.html',1,'']]],
-  ['painterpacker',['PainterPacker',['../d5/dd3/group___painter_packer.html',1,'']]],
   ['painter_20packing',['Painter Packing',['../d8/d2f/group___painter_packing.html',1,'']]],
   ['paths',['Paths',['../de/d6e/group___paths.html',1,'']]]
 ];
