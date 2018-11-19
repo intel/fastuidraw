3fa2d33c157ac41aa312112caa84445fe9137de9
Kevin Rogovin <kevin.rogovin@intel.com>
2018-11-19 22:52:38 +0200

    fastuidraw/text/glyph_render_data: doxytag fixes + explicitely state that invalid_glyph value is for adaptive rendering

--- inc/fastuidraw/text/glyph_render_data.hpp
+++ inc/fastuidraw/text/glyph_render_data.hpp
@@ -42,15 +42,13 @@ namespace fastuidraw
   enum glyph_type
     {
       /*!
-       * Glyph is a coverage glyph, generated
-       * from a GlyphRenderDataCoverage. Glyph is not
+       * Glyph is a coverage glyph. Glyph is not
        * scalable.
        */
       coverage_glyph,
 
       /*!
-       * Glyph is a distance field glyph, generated
-       * from a GlyphRenderDataDistanceField. Glyph
+       * Glyph is a distance field glyph. Glyph
        * is scalable.
        */
       distance_field_glyph,
@@ -65,9 +63,22 @@ namespace fastuidraw
       /*!
        * Tag to indicate invalid glyph type; the value is much
        * larger than the last glyph type to allow for later ABI
-       * compatibility as more glyph types are added.
+       * compatibility as more glyph types are added. Value is
+       * also to indicate to Painter::draw_glyphs() to draw glyphs
+       * adaptively (i.e. choose a renderer based from the size
+       * of the rendered glyphs).
        */
       invalid_glyph = 0x1000,
+
+      /*!
+       * Tag to indicate invalid glyph type; the value is much
+       * larger than the last glyph type to allow for later ABI
+       * compatibility as more glyph types are added. Value is
+       * also to indicate to Painter::draw_glyphs() to draw glyphs
+       * adaptively (i.e. choose a renderer based from the size
+       * of the rendered glyphs).
+       */
+      adaptive_rendering = invalid_glyph,
     };
 
   /*!
@@ -86,12 +97,16 @@ namespace fastuidraw
 
     /*!
      * Ctor.
-     * \param t value to which to initialize m_type, value must be so
+     * \param t value to which to initialize \ref m_type, value must be so
      *          that scalable() returns true
      */
     explicit
     GlyphRender(enum glyph_type t);
 
+    /*!
+     * Ctor. Initializes \ref m_type to \ref invalid_glyph (which is the
+     * same value as \ref adaptive_rendering).
+     */
     GlyphRender(void);
 
     /*!
