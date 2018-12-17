5f00432653c629ffe3943bfe38edd65467c1fa12
Kevin Rogovin <kevin.rogovin@intel.com>
2018-12-17 15:31:22 +0200

    demos/common: slight tweaks for new add_glyphs() methods in GlyphRun

--- demos/common/cast_c_array.hpp
+++ demos/common/cast_c_array.hpp
@@ -13,6 +13,15 @@ cast_c_array(const std::vector<T> &p)
 }
 
 template<typename T>
+fastuidraw::c_array<const T>
+const_cast_c_array(const std::vector<T> &p)
+{
+  return (p.empty()) ?
+    fastuidraw::c_array<const T>() :
+    fastuidraw::c_array<const T>(&p[0], p.size());
+}
+
+template<typename T>
 fastuidraw::c_array<T>
 cast_c_array(std::vector<T> &p)
 {
