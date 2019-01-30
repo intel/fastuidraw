63a04bf985fc3f26ccb23aaf9ab3ce72cf96b73c
Kevin Rogovin <kevin.rogovin@intel.com>
2019-01-30 10:57:50 +0200

    fastuidraw/util/rect: change Rect to a typedef of new template RectT

--- inc/fastuidraw/util/rect.hpp
+++ inc/fastuidraw/util/rect.hpp
@@ -28,9 +28,9 @@ namespace fastuidraw
  */
 
   /*!
-   * Class to specify the geometry of a rounded rectangle.
+   * Class to specify enumerations used by \ref RectT.
    */
-  class Rect
+  class RectEnums
   {
   public:
     enum
@@ -50,21 +50,39 @@ namespace fastuidraw
         maxx_miny_corner = maxx_mask,
         maxx_maxy_corner = maxx_mask | maxy_mask,
       };
+  };
 
+  /*!
+   * Class to specify the geometry of a rectangle.
+   */
+  template<typename T>
+  class RectT:public RectEnums
+  {
+  public:
     /*!
      * Empty ctor; intializes both \ref m_min_point and
      * \ref m_max_point to (0, 0);
      */
-    Rect(void):
-      m_min_point(0.0f, 0.0f),
-      m_max_point(0.0f, 0.0f)
+    RectT(void):
+      m_min_point(T(0), T(0)),
+      m_max_point(T(0), T(0))
+    {}
+
+    /*!
+     * Copy ctor from different rect type
+     */
+    template<typename S>
+    explicit
+    RectT(const RectT<S> &rect):
+      m_min_point(rect.m_min_point),
+      m_max_point(rect.m_max_point)
     {}
 
     /*!
      * Set \ref m_min_point.
      */
-    Rect&
-    min_point(const vec2 &p)
+    RectT&
+    min_point(const vecN<T, 2> &p)
     {
       m_min_point = p;
       return *this;
@@ -73,8 +91,8 @@ namespace fastuidraw
     /*!
      * Set \ref m_min_point.
      */
-    Rect&
-    min_point(float x, float y)
+    RectT&
+    min_point(T x, T y)
     {
       m_min_point.x() = x;
       m_min_point.y() = y;
@@ -84,8 +102,8 @@ namespace fastuidraw
     /*!
      * Set \ref m_max_point.
      */
-    Rect&
-    max_point(const vec2 &p)
+    RectT&
+    max_point(const vecN<T, 2> &p)
     {
       m_max_point = p;
       return *this;
@@ -94,8 +112,8 @@ namespace fastuidraw
     /*!
      * Set \ref m_max_point.
      */
-    Rect&
-    max_point(float x, float y)
+    RectT&
+    max_point(T x, T y)
     {
       m_max_point.x() = x;
       m_max_point.y() = y;
@@ -105,59 +123,59 @@ namespace fastuidraw
     /*!
      * Equivalent to \code m_min_point.x() \endcode
      */
-    float&
+    T&
     min_x(void) { return m_min_point.x(); }
 
     /*!
      * Equivalent to \code m_min_point.x() \endcode
      */
-    float
+    T
     min_x(void) const { return m_min_point.x(); }
 
     /*!
      * Equivalent to \code m_min_point.y() \endcode
      */
-    float&
+    T&
     min_y(void) { return m_min_point.y(); }
 
     /*!
      * Equivalent to \code m_min_point.y() \endcode
      */
-    float
+    T
     min_y(void) const { return m_min_point.y(); }
 
     /*!
      * Equivalent to \code m_max_point.x() \endcode
      */
-    float&
+    T&
     max_x(void) { return m_max_point.x(); }
 
     /*!
      * Equivalent to \code m_max_point.x() \endcode
      */
-    float
+    T
     max_x(void) const { return m_max_point.x(); }
 
     /*!
      * Equivalent to \code m_max_point.y() \endcode
      */
-    float&
+    T&
     max_y(void) { return m_max_point.y(); }
 
     /*!
      * Equivalent to \code m_max_point.y() \endcode
      */
-    float
+    T
     max_y(void) const { return m_max_point.y(); }
 
     /*!
      * Return the named point of the Rect.
      * \param c which corner of the rect.
      */
-    vec2
+    vecN<T, 2>
     point(enum corner_t c) const
     {
-      vec2 return_value;
+      vecN<T, 2> return_value;
 
       return_value.x() = (c & maxx_mask) ? max_x() : min_x();
       return_value.y() = (c & maxy_mask) ? max_y() : min_y();
@@ -172,8 +190,8 @@ namespace fastuidraw
      * \endcode
      * \param tr amount by which to translate
      */
-    Rect&
-    translate(const vec2 &tr)
+    RectT&
+    translate(const vecN<T, 2> &tr)
     {
       m_min_point += tr;
       m_max_point += tr;
@@ -183,15 +201,15 @@ namespace fastuidraw
     /*!
      * Translate the Rect, equivalent to
      * \code
-     * translate(vec2(x, y))
+     * translate(vecN<T, 2>(x, y))
      * \endcode
      * \param x amount by which to translate in x-coordinate
      * \param y amount by which to translate in y-coordinate
      */
-    Rect&
-    translate(float x, float y)
+    RectT&
+    translate(T x, T y)
     {
-      return translate(vec2(x,y));
+      return translate(vecN<T, 2>(x,y));
     }
 
     /*!
@@ -202,8 +220,8 @@ namespace fastuidraw
      * \endcode
      * \param sz size to which to set the Rect
      */
-    Rect&
-    size(const vec2 &sz)
+    RectT&
+    size(const vecN<T, 2> &sz)
     {
       m_max_point = m_min_point + sz;
       return *this;
@@ -213,13 +231,13 @@ namespace fastuidraw
      * Set \ref m_max_point from \ref m_min_point
      * and a size. Equivalent to
      * \code
-     * max_point(min_point() + vec2(width, height))
+     * max_point(min_point() + vecN<T, 2>(width, height))
      * \endcode
      * \param width width to which to set the rect
      * \param height height to which to set the rect
      */
-    Rect&
-    size(float width, float height)
+    RectT&
+    size(T width, T height)
     {
       m_max_point.x() = m_min_point.x() + width;
       m_max_point.y() = m_min_point.y() + height;
@@ -233,7 +251,7 @@ namespace fastuidraw
      * m_max_point - m_min_point
      * \endcode
      */
-    vec2
+    vecN<T, 2>
     size(void) const
     {
       return m_max_point - m_min_point;
@@ -246,8 +264,8 @@ namespace fastuidraw
      * \endcode
      * \param w value to make the width of the rect
      */
-    Rect&
-    width(float w)
+    RectT&
+    width(T w)
     {
       m_max_point.x() = w + m_min_point.x();
       return *this;
@@ -260,8 +278,8 @@ namespace fastuidraw
      * \endcode
      * \param h value to make the height of the rect
      */
-    Rect&
-    height(float h)
+    RectT&
+    height(T h)
     {
       m_max_point.y() = h + m_min_point.y();
       return *this;
@@ -273,7 +291,7 @@ namespace fastuidraw
      * m_max_point.x() - m_min_point.x();
      * \endcode
      */
-    float
+    T
     width(void) const
     {
       return m_max_point.x() - m_min_point.x();
@@ -285,7 +303,7 @@ namespace fastuidraw
      * m_max_point.y() - m_min_point.y();
      * \endcode
      */
-    float
+    T
     height(void) const
     {
       return m_max_point.y() - m_min_point.y();
@@ -295,23 +313,29 @@ namespace fastuidraw
      * Sanitizes the Rect so that both width() and
      * height() are non-negative.
      */
-    Rect&
+    RectT&
     sanitize_size(void)
     {
-      width(t_max(0.0f, width()));
-      height(t_max(0.0f, height()));
+      width(t_max(T(0), width()));
+      height(t_max(T(0), height()));
       return *this;
     }
 
     /*!
      * Specifies the min-corner of the rectangle
      */
-    vec2 m_min_point;
+    vecN<T, 2> m_min_point;
 
     /*!
      * Specifies the max-corner of the rectangle.
      */
-    vec2 m_max_point;
+    vecN<T, 2> m_max_point;
   };
+
+  /*!
+   * Conveniance typedef for RectT<float>
+   */
+  typedef RectT<float> Rect;
+
 /*! @} */
 }
