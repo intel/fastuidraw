8de8f2f1183c8e4dc9727da7f4c90367ae81dc09
Kevin Rogovin <kevin.rogovin@intel.com>
2019-02-14 08:14:17 +0200

    fastuidraw/private/bezier_util: add arc-to-quadratic approximations + rework cubic-to-quadratic approximations

--- src/fastuidraw/private/bezier_util.hpp
+++ src/fastuidraw/private/bezier_util.hpp
@@ -22,6 +22,7 @@
 #include <fastuidraw/util/util.hpp>
 #include <fastuidraw/util/c_array.hpp>
 #include <fastuidraw/util/vecN.hpp>
+#include <fastuidraw/util/math.hpp>
 
 namespace fastuidraw
 {
@@ -78,25 +79,10 @@ namespace fastuidraw
         }
       else
         {
-          /* Compute where the lines [p0, p1] and [p2, p3]
-           * intersect. If the intersection point goes
-           * to far beyond p1 or p3, take the average
-           * of clamping it going out upto 3 times the
-           * length of [p0, p1] or [p2, p3].
-           */
-          vecN<IntermediateFloatType, 2> Cs, Ct;
-          IntermediateFloatType s, t;
+          IntermediateFloatType three(3), four(4);
+          vecN<IntermediateFloatType, 2> q0(pts[0]), q1(pts[1]), q2(pts[2]), q3(pts[3]);
 
-          s = IntermediateFloatType(dot(Jd32, p3 - p0)) / IntermediateFloatType(det);
-          t = IntermediateFloatType(dot(Jd10, p0 - p3)) / IntermediateFloatType(det);
-
-          s = t_max(IntermediateFloatType(-3.0), t_min(s, IntermediateFloatType(3.0)));
-          t = t_max(IntermediateFloatType(-3.0), t_min(t, IntermediateFloatType(3.0)));
-
-          Cs = vecN<IntermediateFloatType, 2>(p0) + s * vecN<IntermediateFloatType, 2>(d10);
-          Ct = vecN<IntermediateFloatType, 2>(p3) - t * vecN<IntermediateFloatType, 2>(d32);
-
-          C = IntermediateFloatType(0.5) * (Cs + Ct);
+          C = (three * q2 - q3 + three * q1 - q0) / four;
         }
 
       vecN<vecN<OutputType, 2>, 3> return_value;
@@ -155,5 +141,146 @@ namespace fastuidraw
       c_array<const vecN<InputType, 2> > q(pts.c_ptr(), 4);
       return split_cubicT<InputType, IntermediateType, OutputType>(q);
     }
+
+    inline
+    float
+    compute_quadratic_cubic_approximate_error(fastuidraw::c_array<const fastuidraw::vec2> p)
+    {
+      /* taking the comptuation from http://caffeineowl.com/graphics/2d/vectorial/cubic2quad01.html,
+       * letting q1 = (3p2 - p3 + 3p1 - p0) / 4, the error between the quadratic [p0, q1, p2] and
+       * the cube [p0, p1, p2, p3] is given by ||p3 - 3p2 + 3p1 - p0 || sqrt(3) / 36. We split
+       * the cubic until that error is achieved and then approximate by q.
+       */
+      const float error_coeff(0.04811252243f);
+      fastuidraw::vec2 error_vec(p[3] - 3.0f * p[2] + 3.0f * p[1] - p[0]);
+      float error(error_coeff * error_vec.magnitude());
+      return error;
+    }
+
+    template<typename Builder>
+    void
+    add_cubic_adaptive(int max_recursion, Builder *B,
+                       c_array<const vec2> p, float tol)
+    {
+      FASTUIDRAWassert(p.size() == 4);
+      if (max_recursion == 0 || compute_quadratic_cubic_approximate_error(p) < tol)
+        {
+          vec2 q1;
+          q1 = (3.0f * p[2] - p[3] + 3.0f * p[1] - p[0]) * 0.25f;
+          B->quadratic_to(q1, p[3]);
+        }
+      else
+        {
+          /* split the cubic */
+          vecN<vecN<vec2, 4>, 2> split;
+
+          split = split_cubicT<float>(p);
+          add_cubic_adaptive(max_recursion - 1, B, split[0], tol);
+          add_cubic_adaptive(max_recursion - 1, B, split[1], tol);
+        }
+    }
+
+    template<typename Builder>
+    void
+    add_arc_as_single_cubic(int max_recursion, Builder *B, float tol,
+                            vec2 from_pt, vec2 to_pt, float angle)
+    {
+      vec2 vp(to_pt - from_pt);
+      vec2 jp(-vp.y(), vp.x());
+      float d(t_tan(angle * 0.25f));
+      vec2 c0, c1;
+
+      vp *= ((1.0f - d * d) / 3.0f);
+      jp *= (2.0f * d / 3.0f);
+      c0 = from_pt + vp - jp;
+      c1 = to_pt - vp - jp;
+
+      add_cubic_adaptive(max_recursion, B,
+                         vecN<vec2, 4>(from_pt, c0, c1, to_pt),
+                         tol);
+    }
+
+    template<typename Builder>
+    void
+    add_arc_as_cubics(int max_recursion, Builder *B, float tol,
+                      vec2 start_pt, vec2 end_pt, vec2 center,
+                      float radius, float start_angle, float angle)
+    {
+      /* One way to approximate an arc with a cubic-bezier is as
+       * follows (taken from GLyphy which is likely take its
+       * computations from Cairo).
+       *
+       *   D = tan(angle / 4)
+       *   p0 = start of arc
+       *   p3 = end of arc
+       *   vp = p3 - p1
+       *   jp = J(vp)
+       *   A = (1 - D^2) / 3
+       *   B = (2 * D) / 3
+       *   p1 = p0 + A * vp - B * jp
+       *   p2 = p1 - A * vp - B * jp
+       *
+       * and the error between the arc and [p0, p1, p2, p3] is given by
+       *
+       *   error <= |vp| * |D|^5 / 54(1 + D^2)
+       *
+       * Now, |angle| < 2 * PI, implies |angle| / 4 < PI / 4 thus |D| < 1, giving
+       *
+       *   error <= |vp| * |D|^5 / 27
+       *
+       * thus we need:
+       *
+       *  |tan(|angle| / 4)|^5 < (27 * tol) / |vp|
+       *
+       * since, tan() is increasing function,
+       *
+       *   |angle| < 4 * pow(atan((27 * tol) / |vp|), 0.20f)
+       *
+       */
+
+      vec2 vp(end_pt - start_pt);
+      vec2 jp(-vp.y(), vp.x());
+      float mag_vp(vp.magnitude());
+      float goal, angle_max;
+
+      if (mag_vp < tol)
+        {
+          B->line_to(end_pt);
+          return;
+        }
+
+      /* half the tolerance for the cubic to quadratic and half
+       * the tolerance for arc to cubic.
+       */
+      tol *= 0.5f;
+
+      goal = t_min(1.0f, (27.0f * tol) / vp.magnitude());
+      angle_max = t_min(FASTUIDRAW_PI, 4.0f * std::pow(fastuidraw::t_atan(goal), 0.20f));
+
+      float angle_remaining(angle);
+      float current_angle(start_angle);
+      float angle_direction(t_sign(angle));
+      float angle_advance(angle_max * angle_direction);
+      vec2 last_pt(start_pt);
+
+      while (t_abs(angle_remaining) > angle_max)
+        {
+          float next_angle(current_angle + angle_advance);
+          float cos_next_angle(t_cos(next_angle));
+          float sin_next_angle(t_sin(next_angle));
+          vec2 next_delta, next_pt;
+
+          next_pt.x() = center.x() + cos_next_angle * radius;
+          next_pt.y() = center.y() + sin_next_angle * radius;
+          add_arc_as_single_cubic(max_recursion, B, tol, last_pt, next_pt, angle_advance);
+
+          current_angle += angle_advance;
+          angle_remaining -= angle_advance;
+          last_pt = next_pt;
+        }
+
+      add_arc_as_single_cubic(max_recursion, B, tol, last_pt, end_pt, angle_remaining);
+    }
+
   }
 }
