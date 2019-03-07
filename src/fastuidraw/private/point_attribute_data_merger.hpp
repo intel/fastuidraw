dbd1fe673127e949f93468d8faaf60442f273c35
Kevin Rogovin <kevin.rogovin@intel.com>
2019-03-07 09:58:53 +0200

    src/fastuidraw: add some missing copyright notices

--- src/fastuidraw/private/point_attribute_data_merger.hpp
+++ src/fastuidraw/private/point_attribute_data_merger.hpp
@@ -1,3 +1,21 @@
+/*!
+ * \file point_attribute_data_merger.hpp
+ * \brief file point_attribute_data_merger.hpp
+ *
+ * Copyright 2019 by Intel.
+ *
+ * Contact: kevin.rogovin@intel.com
+ *
+ * This Source Code Form is subject to the
+ * terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with
+ * this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *
+ * \author Kevin Rogovin <kevin.rogovin@intel.com>
+ *
+ */
+
 #pragma once
 
 #include <fastuidraw/util/util.hpp>
--- src/fastuidraw/util/string_array.cpp
+++ src/fastuidraw/util/string_array.cpp
@@ -1,3 +1,21 @@
+/*!
+ * \file string_array.hpp
+ * \brief file string_array.hpp
+ *
+ * Copyright 2018 by Intel.
+ *
+ * Contact: kevin.rogovin@intel.com
+ *
+ * This Source Code Form is subject to the
+ * terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with
+ * this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *
+ * \author Kevin Rogovin <kevin.rogovin@intel.com>
+ *
+ */
+
 #include <string>
 #include <vector>
 #include <fastuidraw/util/string_array.hpp>
