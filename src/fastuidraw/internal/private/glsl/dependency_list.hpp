62c69ba19ee0f61fc92885f3235f27e5c07ccd00
Kevin Rogovin <kevin.rogovin@intel.com>
2019-04-29 23:22:27 +0300

    fastuidraw/glsl/dependency_list: merge aliased varying together
    This allows for applications to alias varyings to child shader varyings
    reliably in the presence of alias-chains.

--- src/fastuidraw/internal/private/glsl/dependency_list.hpp
+++ src/fastuidraw/internal/private/glsl/dependency_list.hpp
@@ -18,11 +18,13 @@
 
 #pragma once
 
-#include <vector>
 #include <string>
-#include <map>
 #include <algorithm>
+#include <iterator>
 #include <sstream>
+#include <vector>
+#include <map>
+#include <set>
 #include <fastuidraw/util/reference_counted.hpp>
 #include <fastuidraw/util/vecN.hpp>
 #include <fastuidraw/util/string_array.hpp>
@@ -53,33 +55,182 @@ private:
   public:
     reference_counted_ptr<const T> m_shader;
     varying_list m_varyings;
+  };
+
+  class EqClass:public reference_counted<EqClass>::non_concurrent
+  {
+  public:
+    EqClass(void):
+      m_type(varying_list::interpolator_number_types),
+      m_added(false)
+    {}
+
+    std::set<std::string> m_names;
+    enum varying_list::interpolator_type_t m_type;
+    bool m_added;
+  };
+
+  class VaryingTracker
+  {
+  public:
+    void
+    add_to_tracker(c_string prefix, const varying_list &src);
 
     void
-    add_to_varyings(const std::string &name, varying_list *dst) const;
+    add_varyings_from_tracker(varying_list *dst);
+
+  private:
+    typedef reference_counted_ptr<EqClass> EqClassRef;
+
+    static
+    std::string
+    make_name(c_string prefix, c_string name);
+
+    void
+    add_varying(const std::string &name,
+                enum varying_list::interpolator_type_t q);
+
+    void
+    add_alias(const std::string &name, const std::string &src_name);
+
+    std::map<std::string, EqClassRef> m_data;
   };
+
+  static
+  void
+  add_varyings(c_string prefix, const varying_list &src, varying_list *dst);
 
   std::map<std::string, PerShader> m_shaders;
 };
 
+/////////////////////////////////////////////////////
+// DependencyListPrivateT<T>::VaryingTracker methods
+template<typename T>
+std::string
+DependencyListPrivateT<T>::VaryingTracker::
+make_name(c_string prefix, c_string name)
+{
+  FASTUIDRAWassert(name);
+  if (!prefix)
+    {
+      return name;
+    }
+
+  std::ostringstream tmp;
+  tmp << prefix << "_" << name;
+  return tmp.str();
+}
+
 template<typename T>
 void
-DependencyListPrivateT<T>::PerShader::
-add_to_varyings(const std::string &name, varying_list *dst) const
+DependencyListPrivateT<T>::VaryingTracker::
+add_varying(const std::string &name,
+            enum varying_list::interpolator_type_t q)
+{
+  EqClassRef &ref(m_data[name]);
+  if (!ref)
+    {
+      ref = FASTUIDRAWnew EqClass();
+    }
+
+  ref->m_names.insert(name);
+  FASTUIDRAWassert(ref->m_type == varying_list::interpolator_number_types
+                   || ref->m_type == q);
+  ref->m_type = q;
+}
+
+template<typename T>
+void
+DependencyListPrivateT<T>::VaryingTracker::
+add_alias(const std::string &name, const std::string &src_name)
+{
+  EqClassRef &ref1(m_data[name]);
+  EqClassRef &ref2(m_data[src_name]);
+
+  if (!ref1)
+    {
+      ref1 = FASTUIDRAWnew EqClass();
+    }
+
+  if (!ref2)
+    {
+      ref2 = FASTUIDRAWnew EqClass();
+    }
+
+  ref1->m_names.insert(name);
+  ref2->m_names.insert(src_name);
+  if (&ref1 != &ref2)
+    {
+      for (const std::string &nm : ref2->m_names)
+        {
+          ref1->m_names.insert(nm);
+        }
+
+      if (ref1->m_type == varying_list::interpolator_number_types)
+        {
+          ref1->m_type = ref2->m_type;
+        }
+
+      ref2 = ref1;
+    }
+}
+
+template<typename T>
+void
+DependencyListPrivateT<T>::VaryingTracker::
+add_to_tracker(c_string prefix, const varying_list &src)
 {
   for (unsigned int i = 0; i < varying_list::interpolator_number_types; ++i)
     {
       enum varying_list::interpolator_type_t q;
       q = static_cast<enum varying_list::interpolator_type_t>(i);
 
-      for (c_string v : m_varyings.varyings(q))
+      for (c_string v : src.varyings(q))
         {
-          std::ostringstream tmp;
-          tmp << name << "_" << v;
-          dst->add_varying(tmp.str().c_str(), q);
+          add_varying(make_name(prefix, v), q);
+        }
+    }
+
+  c_array<const c_string> names(src.alias_varying_names());
+  c_array<const c_string> src_names(src.alias_varying_source_names());
+  FASTUIDRAWassert(names.size() == src_names.size());
+  for (unsigned int i = 0; i < names.size(); ++i)
+    {
+      add_alias(make_name(prefix, names[i]).c_str(),
+                make_name(prefix, src_names[i]).c_str());
+    }
+}
+
+template<typename T>
+void
+DependencyListPrivateT<T>::VaryingTracker::
+add_varyings_from_tracker(varying_list *dst)
+{
+  for (const auto &e : m_data)
+    {
+      EqClassRef ref(e.second);
+
+      FASTUIDRAWassert(ref);
+      if (!ref->m_added)
+        {
+          FASTUIDRAWassert(!ref->m_names.empty());
+          std::string vname(*ref->m_names.begin());
+
+          ref->m_added = true;
+          FASTUIDRAWassert(ref->m_type != varying_list::interpolator_number_types);
+
+          dst->add_varying(vname.c_str(), ref->m_type);
+          ref->m_names.erase(vname);
+          for (const std::string &nm : ref->m_names)
+            {
+              dst->add_varying_alias(nm.c_str(), vname.c_str());
+            }
         }
     }
 }
 
+/////////////////////////////////////////////
+// DependencyListPrivateT<T> methods
 template<typename T>
 void
 DependencyListPrivateT<T>::
@@ -90,7 +241,7 @@ add_element(c_string pname,
   FASTUIDRAWassert(pname && shader);
 
   std::string name(pname);
-  FASTUIDRAWassert(name.length() > 0 && m_shaders.find(name) == m_shaders.end());
+  FASTUIDRAWassert(m_shaders.find(name) == m_shaders.end());
 
   PerShader &sh(m_shaders[name]);
   sh.m_shader = shader;
@@ -131,11 +282,16 @@ varying_list
 DependencyListPrivateT<T>::
 compute_varyings(const varying_list &combine_with) const
 {
-  varying_list return_value(combine_with);
+  VaryingTracker tracker;
+  varying_list return_value;
+
   for (const auto &v : m_shaders)
     {
-      v.second.add_to_varyings(v.first, &return_value);
+      tracker.add_to_tracker(v.first.c_str(), v.second.m_varyings);
     }
+  tracker.add_to_tracker(nullptr, combine_with);
+  tracker.add_varyings_from_tracker(&return_value);
+
   return return_value;
 }
 
