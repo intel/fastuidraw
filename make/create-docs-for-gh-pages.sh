152edecffcfa832c55bdd4693fe15d6bd1aa1e01
Kevin Rogovin <kevin.rogovin@intel.com>
2019-05-29 12:21:40 +0300

    make/create-docs-for-gh-pages: abort if git tree is not clean

--- make/create-docs-for-gh-pages.sh
+++ make/create-docs-for-gh-pages.sh
@@ -7,6 +7,20 @@
 # and builds the docs from master AND the branch
 # gh-pages has the docs located in docs/html while
 # the docs are built to docs/doxy/html.
+if ! git diff-files --quiet --ignore-submodules --
+then
+    echo >&2 "There are unstaged changes, not updating docs to gh-pages"
+    git diff-files --name-status -r --ignore-submodules -- >&2
+    exit 1
+fi
+
+# Disallow uncommitted changes in the index
+if ! git diff-index --cached --quiet HEAD --ignore-submodules --
+then
+    echo >&2 "There are uncommitted changes, not updating docs to gh-pages"
+    git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
+    exit 1
+fi
 
 git checkout master
 make clean-docs
